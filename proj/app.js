634e60fcbe76703555b19547d7adc531f57cf0d9
Akshat Rana <akshat7010@gmail.com>
2020-08-01 16:27:26 +0530

    login system done

--- proj/app.js
+++ proj/app.js
@@ -1,41 +1,96 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express"), 
+	mongoose = require("mongoose"), 
+	passport = require("passport"), 
+	bodyParser = require("body-parser"), 
+	LocalStrategy = require("passport-local"), 
+	passportLocalMongoose = 
+		require("passport-local-mongoose"), 
+	User = require("./models/user"); 
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+mongoose.set('useNewUrlParser', true); 
+mongoose.set('useFindAndModify', false); 
+mongoose.set('useCreateIndex', true); 
+mongoose.set('useUnifiedTopology', true); 
+mongoose.connect("mongodb://localhost/auth_demo_app"); 
 
-var app = express();
+var app = express(); 
+app.set("view engine", "ejs"); 
+app.use(bodyParser.urlencoded({ extended: true })); 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.use(require("express-session")({ 
+	secret: "Rusty is a dog", 
+	resave: false, 
+	saveUninitialized: false
+})); 
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize()); 
+app.use(passport.session()); 
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+passport.use(new LocalStrategy(User.authenticate())); 
+passport.serializeUser(User.serializeUser()); 
+passport.deserializeUser(User.deserializeUser()); 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+//===================== 
+// ROUTES 
+//===================== 
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// Showing home page 
+app.get("/", function (req, res) { 
+	res.render("home"); 
+}); 
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// Showing secret page 
+app.get("/secret", isLoggedIn, function (req, res) { 
+	res.render("secret"); 
+}); 
 
-module.exports = app;
+// Showing register form 
+app.get("/register", function (req, res) { 
+	res.render("register"); 
+}); 
+
+// Handling user signup 
+app.post("/register", function (req, res) { 
+	var username = req.body.username 
+	var password = req.body.password 
+	User.register(new User({ username: username }), 
+			password, function (err, user) { 
+		if (err) { 
+			console.log(err); 
+			return res.render("register"); 
+		} 
+
+		passport.authenticate("local")( 
+			req, res, function () { 
+			res.render("secret"); 
+		}); 
+	}); 
+}); 
+
+//Showing login form 
+app.get("/login", function (req, res) { 
+	res.render("login"); 
+}); 
+
+//Handling user login 
+app.post("/login", passport.authenticate("local", { 
+	successRedirect: "/secret", 
+	failureRedirect: "/login"
+}), function (req, res) { 
+}); 
+
+//Handling user logout 
+app.get("/logout", function (req, res) { 
+	req.logout(); 
+	res.redirect("/"); 
+}); 
+
+function isLoggedIn(req, res, next) { 
+	if (req.isAuthenticated()) return next(); 
+	res.redirect("/login"); 
+} 
+
+var port = process.env.PORT || 3000; 
+app.listen(port, function () { 
+	console.log("Server Has Started!"); 
+}); 
